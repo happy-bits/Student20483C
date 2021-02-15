06b51bbbe0fc1c23a494a5391f0cd2eaf65faafc
Oscar Olsson <happybits@gmail.com>
2021-02-15 13:56:37 +0100

    -

---
+++ Mod02/Democode/Starter/FourthCoffee.MethodTestHarness/FourthCoffee.MethodTestHarness/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthCoffee.MethodTestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var utilities = new Utilities();
+
+            int number = 123;
+            
+            int number2;
+
+            MyMethod(ref number);
+
+            MyMethod2(out number2);
+
+        }
+
+        private static void MyMethod(ref int number)
+        {
+            //number = 400;
+            number++;
+
+        }
+
+        private static void MyMethod2(out int number)
+        {
+            number = 400;
+            //number++;
+
+        }
+    }
+}
---
+++ Mod02/Democode/Starter/FourthCoffee.MethodTestHarness/FourthCoffee.MethodTestHarness/Utilities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthCoffee.MethodTestHarness
+{
+    public class Utilities
+    {
+        public Utilities() 
+        {
+            // TODO: 02: Invoke the Initialize method.
+            Initialize();
+        }
+
+        // TODO: 01: Define the Initialize method.
+        
+        bool Initialize() // Alt-Enter => hjälper till med allt
+        {
+            var path = GetApplicationPath(); // F12 (dyk in i funktionen)
+
+            return !string.IsNullOrEmpty(path);
+        }
+
+
+
+
+        string GetApplicationPath() // Shift-F12 (var används funktionen?)   
+        {
+            // Ctrl minus = backa
+
+            return Assembly.GetExecutingAssembly().Location;
+        }
+
+    }
+}
