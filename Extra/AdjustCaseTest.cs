bb77eaa6a0f076638e71acfa05e52f19df45c35e
Oscar Olsson <happybits@gmail.com>
2021-02-16 11:48:15 +0100

    -

---
+++ Extra/AdjustCaseTest.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Extra
+{
+    [TestClass]
+    public class AdjustCaseTest
+    {
+        [TestMethod]
+        [DataRow("rhino", "cAt", "rHino")]
+        [DataRow("cat", "RHino", "CAt")]
+        [DataRow("rhino", "aAaaA", "rHinO")]
+        [DataRow("RHINO", "aAaaA", "rHinO")]
+        [DataRow("RHINO", "aAaaAaAaAaA", "rHinO")]   // längre template
+        [DataRow("RHINOoOoO", "aAaaA", "rHinOoOoO")] // längre ord
+        public void adjust_case(string word, string template, string expected)
+        {
+            Assert.AreEqual(expected, AdjustCase(word, template));
+        }
+
+        private string AdjustCase(string word, string template)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
