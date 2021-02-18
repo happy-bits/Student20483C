ea9b529da1b3c74bc16caf162e3910e17c52c110
Oscar Olsson <happybits@gmail.com>
2021-02-18 12:56:07 +0100

    -

---
+++ Mod09/Labfiles/Solution/Exercise 1/Grades.WPF/Controls/StudentPhoto.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+// OO: 1 (sista uppgifterna)
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Grades.WPF
+{
+    /// <summary>
+    /// Interaction logic for StudentPhoto.xaml
+    /// </summary>
+    public partial class StudentPhoto : UserControl
+    {
+        public StudentPhoto()
+        {
+            InitializeComponent();
+        }
+    }
+}
