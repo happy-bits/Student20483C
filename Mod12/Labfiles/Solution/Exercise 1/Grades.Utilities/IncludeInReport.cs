6c2f846e09244c0e3e9a693092ef958eb1d77343
Oscar Olsson <happybits@gmail.com>
2021-02-19 13:41:03 +0100

    -

---
+++ Mod12/Labfiles/Solution/Exercise 1/Grades.Utilities/IncludeInReport.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Utilities
+{
+
+    // OO: 1a-f
+
+    // OO: Attributet kan användas på ett fält eller property. Flera av dessa attribut får inte vara på samma fält/prop
+    // OO: Måste ärva från System.Attribute
+
+    /// <summary>
+    /// Custom attribute that specifies whether a field or property should be included in a report
+    /// </summary>
+    
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+    public class IncludeInReportAttribute : Attribute
+    {
+        private bool _include;
+
+        // Public properties that specify how an included item should be formatted
+        public bool Underline { get; set; }
+        public bool Bold { get; set; }
+
+        // Public property that specifies a label (if any) for the item
+        public string Label { get; set; }
+
+        public IncludeInReportAttribute()
+        {
+            this._include = true;
+            this.Underline = false;
+            this.Bold = false;
+            this.Label = string.Empty;
+        }
+
+        public IncludeInReportAttribute(bool includeInReport)
+        {
+            this._include = includeInReport;
+            this.Underline = false;
+            this.Bold = false;
+            this.Label = string.Empty;
+        }
+    }
+}
